72a3ca7719132006e08534a3d6eda5d67a341bb5
arcadev <arcacorini@gmail.com>
2025-10-18 02:26:20 -0400

    pipes

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Hilux',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: 3,
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOnById(id: number) {
+    return this.cars.find((e) => e.id == id);
+  }
+}
